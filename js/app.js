fadf1837f84d03beb8227f66921a65a858ce0d13
James Volmert <james.volmert@gmail.com>
2016-01-31 13:18:54 -0700

    working on Font

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
-/**
- * Created by jamesvolmert on 1/31/16.
- */
+$(document).ready(function () {
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.sportradar.us/mlb-t5/league/full_rosters.json?api_key=p3tgpnbk5tujzqkpenczuwtm',
+        dataType: 'json',
+        success: function(data) {
+            console.log(data);
+        }
+    })
+}); //end of jQuery script
