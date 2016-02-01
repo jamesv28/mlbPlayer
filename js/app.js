b620034f1062082466553d3cfffc78b1f237c99f
James Volmert <james.volmert@gmail.com>
2016-01-31 19:03:34 -0700

    worked on index

--- js/app.js
+++ js/app.js
@@ -3,8 +3,14 @@ $(document).ready(function () {
         method: 'GET',
         url: 'https://api.sportradar.us/mlb-t5/league/full_rosters.json?api_key=p3tgpnbk5tujzqkpenczuwtm',
         dataType: 'json',
+        //crossDomain: true,
         success: function(data) {
-            console.log(data);
+            /**
+             * fidn the plaer id by going through the data object
+             * in rosters > players to see if the input matches a player
+             * and then make another ajax request to go into players/ players id stats
+             */
+
         }
     })
 }); //end of jQuery script
