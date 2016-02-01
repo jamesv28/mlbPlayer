0fcb083736fc43d0de9cb8484613e3deec4cb77b
James Volmert <james.volmert@gmail.com>
2016-02-01 16:45:59 -0700

    started manipulating the dom

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
-
+$('#findPlayer').on('click', function(e) {
+    e.preventDefault();
+    var playerStats = $('#playerName').val();
+    for(var i = 0; i < obj.playerInfo.length; i++) {
+        if(playerStats === obj.playerInfo[i].full_name) {
+            $('#playerContent').html('');
+            var string = '';
+            var fullName = document.createElement('h2');
+            var playerContent = document.getElementById('playerContent');
+            playerContent.appendChild(fullName);
+            fullName.innerHTML = obj.playerInfo[i].full_name;
+            var position = document.createElement('p');
+            playerContent.appendChild(position);
+            position.innerHTML = '<strong>Position: </strong> ' + obj.playerInfo[i].position;
+        }
+    }
+});
     $(document).ready(function () {
 
         console.log(obj.playerInfo);
