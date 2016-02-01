0e6f4fe0cf8620eea1eef62ebb5ca3665075d84d
James Volmert <james.volmert@gmail.com>
2016-02-01 15:21:01 -0700

    loaded the data

--- js/app.js
+++ js/app.js
@@ -1,28 +1,20 @@
-$(document).ready(function () {
 
-    var settings = {
-         "async": true,
-         "crossDomain": true,
-        "url": 'https://api.sportradar.us/mlb-t5/league/full_rosters.json?api_key=p3tgpnbk5tujzqkpenczuwtm',
-        "method": "GET",
-        "dataType": 'jsonp'
-    };
+    $(document).ready(function () {
 
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-    });
-    //$.ajax({
-    //    method: 'GET',
-    //    url: 'https://api.sportradar.us/mlb-t5/league/full_rosters.json?api_key=p3tgpnbk5tujzqkpenczuwtm',
-    //    dataType: 'json',
-    //    //crossDomain: true,
-    //    success: function(data) {
-    //        /**
-    //         * fidn the plaer id by going through the data object
-    //         * in rosters > players to see if the input matches a player
-    //         * and then make another ajax request to go into players/ players id stats
-    //         */
-    //
-    //    }
-    //})
+        console.log(obj.playerInfo);
+
+
+
+
+
+
+        /**
+         * players:
+         * Pablo sandoval
+         * salvador perez
+         * mike trout
+         * kolten wong
+         * Nolan Arenado
+         * mathew Carpenter
+         */
 }); //end of jQuery script
